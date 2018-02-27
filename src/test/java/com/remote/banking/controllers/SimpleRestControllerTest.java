02afbe2c7a0555212821edb9883e49ac8d61c0b8
Evgenii_Lartcev <i.dolende@gmail.com>
2018-02-27 14:40:31 +0300

    very simple test

---
+++ src/test/java/com/remote/banking/controllers/SimpleRestControllerTest.java
@@ -0,0 +1,32 @@
+package com.remote.banking.controllers;
+
+import com.remote.banking.TestApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(SimpleRestController.class)
+@ContextConfiguration(classes = {TestApplication.class})
+public class SimpleRestControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void hello() throws Exception {
+        mockMvc.perform(get("/rest/v1/hello/world"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_PLAIN))
+                .andExpect(content().string("Hello, world!"));
+    }
+}
