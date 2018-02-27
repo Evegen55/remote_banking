1db3764246e3bd5f480108c27c6a338f15056df2
Evgenii_Lartcev <i.dolende@gmail.com>
2018-02-27 14:06:09 +0300

    works fine

--- src/main/java/com/remote/banking/controllers/SimpleRestController.java
+++ src/main/java/com/remote/banking/controllers/SimpleRestController.java
@@ -1,4 +1,21 @@
 package com.remote.banking.controllers;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("${api.root}/hello")
 public class SimpleRestController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/world")
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.CONTENT_DISPOSITION)
+                .body("Hello, world!");
+    }
 }
