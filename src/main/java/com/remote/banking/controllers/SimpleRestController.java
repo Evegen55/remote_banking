50256e7e2dafed6e0fafad2df35b03ebe743e866
Evgenii_Lartcev <i.dolende@gmail.com>
2018-02-27 14:19:59 +0300

    logging

--- src/main/java/com/remote/banking/controllers/SimpleRestController.java
+++ src/main/java/com/remote/banking/controllers/SimpleRestController.java
@@ -1,5 +1,7 @@
 package com.remote.banking.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("${api.root}/hello")
 public class SimpleRestController {
 
+    private final static Logger LOGGER = LoggerFactory.getLogger(SimpleRestController.class);
+
     @RequestMapping(method = RequestMethod.GET, value = "/world")
     public ResponseEntity<String> hello() {
+        LOGGER.info("Simple REST controller has been invoked");
         return ResponseEntity.ok()
                 .contentType(MediaType.APPLICATION_JSON)
                 .header(HttpHeaders.CONTENT_DISPOSITION)
