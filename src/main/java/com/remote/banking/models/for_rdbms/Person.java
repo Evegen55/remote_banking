012c3c5361aad2b728df8122c29111062a9b9aeb
Evgenii_Lartcev <i.dolende@gmail.com>
2018-03-01 20:31:27 +0300

    delete old Date api

--- src/main/java/com/remote/banking/models/for_rdbms/Person.java
+++ src/main/java/com/remote/banking/models/for_rdbms/Person.java
@@ -5,7 +5,6 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 
