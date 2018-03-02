388457c4441039f7f09184115744ad90fdc8474c
Evgenii_Lartcev <i.dolende@gmail.com>
2018-03-02 18:41:27 +0300

    prod version of REST API for persons

--- src/main/java/com/remote/banking/controllers/UserController.java
+++ src/main/java/com/remote/banking/controllers/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("${api.root}/hello")
+@RequestMapping("${api.root}")
 public class UserController {
 
     @Autowired
