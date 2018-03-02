2830f340da3aa690f873fefcbafafca31d010b70
Evgenii_Lartcev <i.dolende@gmail.com>
2018-03-02 17:29:37 +0300

    actually controller for users works fine but in general it has to be tested

---
+++ src/test/java/com/remote/banking/models/dao/UserDAOTest.java
@@ -0,0 +1,26 @@
+package com.remote.banking.models.dao;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+// TODO: 3/2/2018 create test
+public class UserDAOTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void setup() {
+    }
+
+    @Test
+    public void findById() {
+    }
+
+    @Test
+    public void findAllPersons() {
+    }
+}
