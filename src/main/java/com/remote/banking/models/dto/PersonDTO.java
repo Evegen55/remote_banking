2986de9d558fe317131f1844ac2b313c03df6ec6
Evgenii_Lartcev <i.dolende@gmail.com>
2018-03-06 17:58:13 +0300

    Data Transfer Object design pattern

---
+++ src/main/java/com/remote/banking/models/dto/PersonDTO.java
@@ -0,0 +1,36 @@
+package com.remote.banking.models.dto;
+
+import java.time.LocalDate;
+
+public class PersonDTO {
+
+    public final String firstName;
+
+    public final String lastName;
+
+    public final LocalDate dateOfBirth;
+
+    public final String gender;
+
+    public final String emailsList;
+
+    public PersonDTO(String firstName, String lastName, LocalDate dateOfBirth, String gender, String emailsList) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.emailsList = emailsList;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("PersonDTO{");
+        sb.append("firstName='").append(firstName).append('\'');
+        sb.append(", lastName='").append(lastName).append('\'');
+        sb.append(", dateOfBirth=").append(dateOfBirth);
+        sb.append(", gender='").append(gender).append('\'');
+        sb.append(", emailsList=").append(emailsList);
+        sb.append('}');
+        return sb.toString();
+    }
+}
