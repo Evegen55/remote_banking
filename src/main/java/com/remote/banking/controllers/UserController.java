fbc3aa6e632d8c481c3630d5e8cd027567ced553
Evgenii_Lartcev <i.dolende@gmail.com>
2018-03-06 18:29:22 +0300

    REST API as pretty printed JSON with links but without self-links for Users and Emails. TODO - REST API for Google Accounts

--- src/main/java/com/remote/banking/controllers/UserController.java
+++ src/main/java/com/remote/banking/controllers/UserController.java
@@ -2,13 +2,13 @@ package com.remote.banking.controllers;
 
 import com.google.gson.GsonBuilder;
 import com.remote.banking.models.dao.UserDAO;
+import com.remote.banking.models.dto.EmailsDTO;
 import com.remote.banking.models.dto.PersonDTO;
 import com.remote.banking.models.for_rdbms.Emails;
 import com.remote.banking.models.for_rdbms.Person;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,24 +29,31 @@ public class UserController {
     @Autowired
     private UserDAO userDAO;
 
+    // TODO: 3/6/2018 get web path parameters from settings or context
     private String formBaseRESTFulURLForUsers() {
         return "http://localhost:8080/rest/v1" + "/users";
     }
 
+    private String formBaseRESTFulURLForUser(final int idperson) {
+        return formBaseRESTFulURLForUsers() + "/" + idperson;
+    }
+
     private String formBaseRESTFulURLForUsersEmails(final int idperson) {
-        return formBaseRESTFulURLForUsers() + "/" + idperson + "/emails";
+        return formBaseRESTFulURLForUser(idperson) + "/emails";
     }
 
     private static final GsonBuilder PRETTY_PRINTING_BUILDER = new GsonBuilder().setPrettyPrinting();
 
     @RequestMapping(method = RequestMethod.GET, value = "/users")
     public ResponseEntity<String> getUserInfo() {
-        LOGGER.info("Start retrieve all persons from database");
-        final List<Person> all = userDAO.findAllPersons();
+        LOGGER.info("Start retrieve allPersons persons from database");
+        final List<Person> allPersons = userDAO.findAllPersons();
         LOGGER.info("All persons from database have been retrieved");
-        List<PersonDTO> personDTOS = all.stream().map(person -> new PersonDTO(person.getFirstName(),
-                person.getLastName(), person.getDateOfBirth(),
-                person.getGender(), formBaseRESTFulURLForUsersEmails(person.getIdperson()))).collect(toList());
+        final List<PersonDTO> personDTOS = allPersons.stream()
+                .map(person -> new PersonDTO(person.getFirstName(), person.getLastName(),
+                        person.getDateOfBirth(), person.getGender(),
+                        formBaseRESTFulURLForUsersEmails(person.getIdperson())))
+                .collect(toList());
         final String toJson = PRETTY_PRINTING_BUILDER.create().toJson(personDTOS);
         return ResponseEntity.ok()
                 .contentType(MediaType.APPLICATION_JSON)
@@ -59,9 +66,9 @@ public class UserController {
         LOGGER.info("Start retrieve person with id {} from database", idperson);
         final Person personById = userDAO.findById(idperson);
         LOGGER.info("Person {} retrieved from database", personById);
-        // TODO: 3/6/2018 get web path parameters from settings or context
         final PersonDTO personDTO = new PersonDTO(personById.getFirstName(), personById.getLastName(),
-                personById.getDateOfBirth(), personById.getGender(), formBaseRESTFulURLForUsersEmails(personById.getIdperson()));
+                personById.getDateOfBirth(), personById.getGender(),
+                formBaseRESTFulURLForUsersEmails(personById.getIdperson()));
         final String toJson = PRETTY_PRINTING_BUILDER.create().toJson(personDTO);
         return ResponseEntity.ok()
                 .contentType(MediaType.APPLICATION_JSON)
@@ -72,10 +79,15 @@ public class UserController {
     public ResponseEntity<String> getUserEmails(@PathVariable int idperson) {
         LOGGER.info("Start retrieve all emails for person with id {} from database", idperson);
         final Person byId = userDAO.findById(idperson);
-        List<Emails> emailsList = byId.getEmailsList();
+        final List<Emails> emailsList = byId.getEmailsList();
         LOGGER.info("Emails with size {} retrieved from database", emailsList.size());
+        final List<EmailsDTO> emailsDTOS = emailsList.stream()
+                .map(email -> new EmailsDTO(email.getEmail(), formBaseRESTFulURLForUsers() + " TODO",
+                        formBaseRESTFulURLForUser(email.getPersonIdperson().getIdperson())))
+                .collect(toList());
+        final String toJson = PRETTY_PRINTING_BUILDER.create().toJson(emailsDTOS);
         return ResponseEntity.ok()
                 .contentType(MediaType.APPLICATION_JSON)
-                .body(emailsList.toString());
+                .body(toJson);
     }
 }
