2bd4c2140b9d05cf5f03c257d68e1f70fde23718
Evgenii_Lartcev <i.dolende@gmail.com>
2018-03-06 18:10:20 +0300

    works fine for users RESTFull API TODO - for user's emails and google accounts

--- src/main/java/com/remote/banking/controllers/UserController.java
+++ src/main/java/com/remote/banking/controllers/UserController.java
@@ -1,6 +1,8 @@
 package com.remote.banking.controllers;
 
+import com.google.gson.GsonBuilder;
 import com.remote.banking.models.dao.UserDAO;
+import com.remote.banking.models.dto.PersonDTO;
 import com.remote.banking.models.for_rdbms.Emails;
 import com.remote.banking.models.for_rdbms.Person;
 import org.slf4j.Logger;
@@ -16,6 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+import static java.util.stream.Collectors.toList;
+
 @RestController
 @RequestMapping("${api.root}")
 public class UserController {
@@ -25,27 +29,45 @@ public class UserController {
     @Autowired
     private UserDAO userDAO;
 
+    private String formBaseRESTFulURLForUsers() {
+        return "http://localhost:8080/rest/v1" + "/users";
+    }
+
+    private String formBaseRESTFulURLForUsersEmails(final int idperson) {
+        return formBaseRESTFulURLForUsers() + "/" + idperson + "/emails";
+    }
+
+    private static final GsonBuilder PRETTY_PRINTING_BUILDER = new GsonBuilder().setPrettyPrinting();
+
     @RequestMapping(method = RequestMethod.GET, value = "/users")
     public ResponseEntity<String> getUserInfo() {
         LOGGER.info("Start retrieve all persons from database");
         final List<Person> all = userDAO.findAllPersons();
         LOGGER.info("All persons from database have been retrieved");
+        List<PersonDTO> personDTOS = all.stream().map(person -> new PersonDTO(person.getFirstName(),
+                person.getLastName(), person.getDateOfBirth(),
+                person.getGender(), formBaseRESTFulURLForUsersEmails(person.getIdperson()))).collect(toList());
+        final String toJson = PRETTY_PRINTING_BUILDER.create().toJson(personDTOS);
         return ResponseEntity.ok()
                 .contentType(MediaType.TEXT_PLAIN)
                 .header(HttpHeaders.CONTENT_DISPOSITION)
-                .body(all.toString());
+                .body(toJson);
     }
 
 
     @RequestMapping(method = RequestMethod.GET, value = "/users/{idperson}")
     public ResponseEntity<String> getUserInfo(@PathVariable int idperson) {
         LOGGER.info("Start retrieve person with id {} from database", idperson);
-        final Person byId = userDAO.findById(idperson);
-        LOGGER.info("Person {} retrieved from database", byId);
+        final Person personById = userDAO.findById(idperson);
+        LOGGER.info("Person {} retrieved from database", personById);
+        // TODO: 3/6/2018 get web path parameters from settings or context
+        final PersonDTO personDTO = new PersonDTO(personById.getFirstName(), personById.getLastName(),
+                personById.getDateOfBirth(), personById.getGender(), formBaseRESTFulURLForUsersEmails(personById.getIdperson()));
+        final String toJson = PRETTY_PRINTING_BUILDER.create().toJson(personDTO);
         return ResponseEntity.ok()
                 .contentType(MediaType.TEXT_PLAIN)
                 .header(HttpHeaders.CONTENT_DISPOSITION)
-                .body(byId.toString());
+                .body(toJson);
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/users/{idperson}/emails")
