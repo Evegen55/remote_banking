6483c063fd4c8d966b0d177fa0f04a727c6e9639
Evgenii_Lartcev <i.dolende@gmail.com>
2018-03-27 19:03:14 +0300

    receiver for jms improved

--- src/main/java/com/remote/banking/jms/Receiver.java
+++ src/main/java/com/remote/banking/jms/Receiver.java
@@ -1,14 +1,18 @@
 package com.remote.banking.jms;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Receiver {
 
+    private final static Logger LOGGER = LoggerFactory.getLogger(Receiver.class);
+
     @JmsListener(destination = "mailbox", containerFactory = "myFactory")
-    public void receiveMessage(String personDTO) {
-        System.out.println("Received <" + personDTO + ">");
+    public void receiveMessage(final String personDTO) {
+        LOGGER.info("Received <" + personDTO + ">");
     }
 
 }
